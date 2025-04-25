71ee2b130c8b2062af629e86f1310b0e6abfc755
Juan S Lemos L <juansebastianlemos45@gmail.com>
2025-04-25 14:58:18 -0500

    Use of lion-lib
    After npm instalation, here is use lion-lib

---
+++ npm-demo/index.js
@@ -0,0 +1,7 @@
+const lion = require('lion-lib');
+
+const sumar = lion.add(5, 3);
+const multiplicar = lion.multiply(5, 3);
+
+console.log(`La suma de 5 + 3 es: ${sumar}`);
+console.log(`La multiplicación de 5 * 3 es: ${multiplicar}`);
